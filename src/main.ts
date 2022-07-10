7e7213e5a28dfb7467f47c33c21cac589646f578
= <=>
2022-07-10 14:43:20 +0300

    add server applyMiddleware

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+const express = require('express');
+const cors = require('cors');
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+
+const PORT = process.env.PORT || 4000
 
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers
 });
+const app = express();
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const corsOptions = {
+  origin: 'https://e-commerce-store-client.herokuapp.com',
+  credentials: true
+}
+app.use(cors(corsOptions));
+
+server.applyMiddleware({
+  app,
+  path: '/',
+  cors: false,
+})
+
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
